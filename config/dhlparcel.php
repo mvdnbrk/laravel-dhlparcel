d26a0e255dda4941c011a94a790c145e1a22b820
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-12 11:58:15 +0100

    Updates config.

--- config/dhlparcel.php
+++ config/dhlparcel.php
@@ -2,6 +2,15 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | DHL Parcel credentials
+    |--------------------------------------------------------------------------
+    |
+    | Here you may provide your API token.
+    |
+    */
+
     'id' => env('DHLPARCEL_ID'),
 
     'secret' => env('DHLPARCEL_SECRET'),
