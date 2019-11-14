e85b857143d0ee9dbf9c5cd14adebc859c7741af
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-14 21:32:56 +0100

    Adds test.

--- tests/DhlParcelServiceProviderTest.php
+++ tests/DhlParcelServiceProviderTest.php
@@ -4,6 +4,7 @@ namespace Mvdnbrk\Laravel\Tests;
 
 use Mvdnbrk\DhlParcel\Client;
 use Mvdnbrk\Laravel\DhlParcelClientAdapter;
+use Mvdnbrk\Laravel\Facades\DhlParcel as DhlParcelFacade;
 
 class DhlParcelServiceProviderTest extends TestCase
 {
@@ -17,5 +18,11 @@ class DhlParcelServiceProviderTest extends TestCase
     public function it_can_resolve_the_dhl_parcel_client_adapter()
     {
         $this->assertInstanceOf(DhlParcelClientAdapter::class, app('dhlparcel.adapter'));
+    }
+
+    /** @test */
+    public function it_can_use_the_facade()
+    {
+        $this->assertNull(DhlParcelFacade::getAccountId());
     }
 }
