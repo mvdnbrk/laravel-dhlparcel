e3eab15c76b16e5f61e3d70f2e6533a00ed9e8f9
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-14 20:33:42 +0100

    Implement the ServiceProvider.

--- src/DhlParcelServiceProvider.php
+++ src/DhlParcelServiceProvider.php
@@ -2,20 +2,12 @@
 
 namespace Mvdnbrk\Laravel;
 
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
+use Mvdnbrk\DhlParcel\Client;
 
-class DhlParcelServiceProvider extends ServiceProvider
+class DhlParcelServiceProvider extends ServiceProvider implements DeferrableProvider
 {
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
     /**
      * Register the application services.
      *
@@ -24,5 +16,65 @@ class DhlParcelServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/dhlparcel.php', 'dhlparcel');
+
+        $this->registerDhlClient();
+        $this->registerDhlAdapter();
+
+        $this->registerPublishing();
+    }
+
+    /**
+     * Register the DHL Parcel Client.
+     *
+     * @return void
+     */
+    protected function registerDhlClient()
+    {
+        $this->app->singleton(Client::class, function () {
+            return (new Client)->setUserId(config('dhlparcel.id'))->setApiKey(config('dhlparcel.secret'));
+        });
+
+        $this->app->alias(Client::class, 'dhlparcel');
+    }
+
+    /**
+     * Register the DHL Parcel Client Adapter.
+     *
+     * @return void
+     */
+    protected function registerDhlAdapter()
+    {
+        $this->app->singleton(DhlParcelClientAdapter::class, function () {
+            return new DhlParcelClientAdapter(app('dhlparcel'));
+        });
+
+        $this->app->alias(DhlParcelClientAdapter::class, 'dhlparcel.adapter');
+    }
+
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
+    protected function registerPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/dhlparcel.php' => config_path('dhlparcel.php'),
+            ], 'dhlparcel-config');
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'dhlparcel',
+            'dhlparcel.adapter',
+        ];
     }
 }
